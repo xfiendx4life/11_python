b6427d7947b60c2fa4974cfed786181eb70cddb9
xfiendx4life <xfiendx4life@gmail.com>
2020-02-03 17:13:52 +0300

    First unittest added

---
+++ Flask + data_base/project/tests/test_req.py
@@ -0,0 +1,45 @@
+# project/test_req.py
+
+import unittest
+import os
+
+from project import app
+from project.models import db, User, Note
+from project.views import *
+
+TEST_DB = 'test.db'
+
+class RequestTests(unittest.TestCase):
+	
+	@classmethod
+	def setUpClass(cls):
+		app.config['TESTING'] = True
+		app.config['WTF_CSRF_ENABLED'] = False
+		app.config['DEBUG'] = False
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + \
+			os.path.join(app.config['BASEDIR'], TEST_DB)
+		app.secret_key = 'staytrue'
+		cls.app = app.test_client()
+
+	def setUp(self):
+		db.create_all()
+
+	def tearDown(self):
+		db.drop_all()
+
+
+	def test_main_page(self):
+		response = self.app.get('/', follow_redirects=True)
+		self.assertEqual(response.status_code, 200)
+
+	def test_refgistration(self):
+		u = User(username='Cyberslav', password='1234567', email='cyber@mail.ru')
+		db.session.add(u)
+		db.session.commit()
+		response = self.app.post('/login', data=dict(username='Cyberslav', password='1234567'), follow_redirects=True)
+		with self.app.session_transaction() as sess:
+			self.assertEqual(sess['username'], 'Cyberslav')
+
+
+if __name__ == "__main__":
+    unittest.main()
